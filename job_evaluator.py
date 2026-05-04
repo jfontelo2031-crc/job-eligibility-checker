8cf615a7cdbe74f0d08382af6ce199b70ec89277
HannsLeviLetada <hlletada2031@crc.pshs.edu.ph>
2026-05-04 21:29:19 +0800

    Update job_evaluator.py

--- job_evaluator.py
+++ job_evaluator.py
@@ -1,3 +1,9 @@
 def display_error_message():
   print(""Error: Invalid input. Please try again.")
   
+def is_number(value):
+    valid = True
+    for character in value:
+        if character < "0" or character > "9":
+            valid = False
+    return valid        
