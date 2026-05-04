48642ffe21cf2fa3ed86bcc565b3f891941795a1
laroy2031-hash <laroy2031@crc.pshs.edu.ph>
2026-05-04 21:34:42 +0800

    Update job_evaluator.py

--- job_evaluator.py
+++ job_evaluator.py
@@ -1,9 +1,17 @@
 def display_error_message():
-  print(""Error: Invalid input. Please try again.")
+  print("Error: Invalid input. Please try again.")
   
 def is_number(value):
     valid = True
     for character in value:
         if character < "0" or character > "9":
             valid = False
-    return valid        
+    return valid  
+
+def get_applicant_info():
+    while True:
+        age_input = input("Enter your age: ")
+        if age_input != "" and is_number(age_input):
+            age = int(age_input)
+            break
+        display_error_message()
